c8ffd4ef56d9e3fe4773f0cfac607c4657dac3b6
devgiljong <calf-love@naver.com>
2024-01-19 02:34:52 +0900

    :sparkles: Initiator

---
+++ di-next/src/core/initiator.ts
@@ -0,0 +1,35 @@
+import { DateTimeSource } from '../adapters/dateTimeSource';
+import { UuidGenerator } from '../adapters/uuidGenderator';
+import { EventHandler } from '../effects/eventHandler';
+import { Logger } from '../effects/logger';
+import { Timer } from '../effects/timer';
+
+export class AppInitiator {
+  constructor(
+    private dateTimeSource: DateTimeSource,
+    private idGenerator: UuidGenerator,
+    private clickHandler: EventHandler<MouseEvent>,
+    private logger: Logger,
+    private timer: Timer,
+    private env: Window,
+  ) {}
+
+  private greet = (): void => this.logger.log('Hello world!');
+  private setupTimer = (): void => this.timer.invokeEvery(this.greet, 5000);
+  private registerClicks = (): void =>
+    this.clickHandler.on('click', this.handleClick);
+
+  private handleClick = (e: MouseEvent): void => {
+    const position = [e.pageX, e.pageY];
+    const datetime = this.dateTimeSource.toString();
+    const eventId = this.idGenerator.generate();
+    this.env.alert(
+      `${eventId}, ${datetime}: Mouse was clicked at ${position} `,
+    );
+  };
+
+  public init = (): void => {
+    this.setupTimer();
+    this.registerClicks();
+  };
+}
