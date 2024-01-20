feea483113690c345af81fd3f81bcd39da07f9f1
devgiljong <calf-love@naver.com>
2024-01-21 02:07:34 +0900

    :sparkles: Add

--- clean/app/layout.tsx
+++ clean/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import '@/src/@shared/style/globals.css';
+import { QueryProvider } from '@/src/@infrastructure/store/server/query-provider';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
--- clean/src/@infrastructure/store/server/query-provider.tsx
+++ clean/src/@infrastructure/store/server/query-provider.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
