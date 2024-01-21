d755faf82196a74c9e48c1ec49564444ecfca294
devgiljong <calf-love@naver.com>
2024-01-21 14:19:51 +0900

    :sparkles: Add

--- clean/src/@infrastructure/store/server/api/log/use-log-mutation.ts
+++ clean/src/@infrastructure/store/server/api/log/use-log-mutation.ts
@@ -4,7 +4,6 @@ import { useMutation } from '@tanstack/react-query';
 export const useLogMutation = () => {
   return useMutation({
     mutationFn: async (log: any) => {
-      console.log('매개변수', log);
       const result = await fetcher.post('/api/log', {
         body: JSON.stringify(log),
       });
