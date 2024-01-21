652fb50cf1477fa6e96728720755ed7207df6ca2
devgiljong <calf-love@naver.com>
2024-01-21 17:29:06 +0900

    :sparkles: Pubsub

--- clean/src/@infrastructure/pub-sub-example/provider.tsx
+++ clean/src/@infrastructure/pub-sub-example/provider.tsx
@@ -6,34 +6,21 @@ import { PubSubManager } from '@/src/@packages/pub-sub/core/core';
 
 export const pubSubManager = new PubSubManager<MyEvent>();
 
-interface PubSubProps extends React.PropsWithChildren {
-  handler: EventHandlersMap<MyEvent>;
-}
-
-const PubSubWrapper = ({ children, handler }: PubSubProps) => {
-  React.useEffect(() => {
-    pubSubManager.initiate(handler);
-  }, [handler]);
-  return <>{children}</>;
-};
-
 export const PubSubProvider = ({ children }: React.PropsWithChildren) => {
-  return (
-    <PubSubWrapper
-      handler={{
-        CREATE_USER: [
-          (event) => {
-            console.log('생성하기 유저', event);
-          },
-        ],
-        DELETE_USER: [
-          (event) => {
-            console.log('지우기 유저', event);
-          },
-        ],
-      }}
-    >
-      {children}
-    </PubSubWrapper>
-  );
+  React.useEffect(() => {
+    const handlers: EventHandlersMap<MyEvent> = {
+      CREATE_USER: [
+        (event) => {
+          console.log('생성하기 유저', event);
+        },
+      ],
+      DELETE_USER: [
+        (event) => {
+          console.log('지우기 유저', event);
+        },
+      ],
+    };
+    pubSubManager.initiate(handlers);
+  }, []);
+  return children;
 };
