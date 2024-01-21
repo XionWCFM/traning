7b13c17908a2791116a63c2fc22fbec2f34521f0
devgiljong <calf-love@naver.com>
2024-01-21 18:30:59 +0900

    :sparkles: Ad
    d

--- clean/app/_component/button.tsx
+++ clean/app/_component/button.tsx
@@ -5,29 +5,26 @@ import { MyEvent } from '@/src/@infrastructure/pub-sub-example/domain';
 import { pubSubManager } from '@/src/@infrastructure/pub-sub-example/provider';
 import { useRootStore } from '@/src/@infrastructure/store/client/client-store';
 import { EventHandler } from '@/src/@packages/pub-sub/core/type';
+import React from 'react';
 
 interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {}
 export const Button = (prop: ButtonProps) => {
   const { track } = useLogger();
   const store = useRootStore();
 
+  React.useEffect(() => {
+    const handler: EventHandler<MyEvent> = (event) => {
+      console.log('handler event');
+    };
+    pubSubManager.subscribe('CREATE_USER', handler);
+    return () => pubSubManager.unsubscribe('CREATE_USER', handler);
+  }, []);
+
   return (
     <button
       onClick={() => {
-        console.group('구독끊기');
-        const handler: EventHandler<MyEvent> = (event) => {
-          console.log('크리에이트 생성 동작함', event);
-        };
-        pubSubManager.subscribe('CREATE_USER', handler);
-        pubSubManager.publish({
-          type: 'CREATE_USER',
-          userName: 'hello world',
-        });
-        pubSubManager.unsubscribe('CREATE_USER', handler);
-        pubSubManager.publish({
-          type: 'CREATE_USER',
-          userName: '이건 동작하면안돼',
-        });
+        console.group('이벤트를 봅시다');
+        pubSubManager.publish({ type: 'CREATE_USER', userName: 'moonjae' });
 
         console.groupEnd();
         track(
