c571b37ded998c577b88ffecef9cd806690119dc
devgiljong <calf-love@naver.com>
2024-01-21 14:19:14 +0900

    :sparkles: Add

--- clean/src/@infrastructure/store/server/query-provider.tsx
+++ clean/src/@infrastructure/store/server/query-provider.tsx
@@ -3,16 +3,7 @@ import React from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 export const QueryProvider = ({ children }: React.PropsWithChildren) => {
-  const [queryClientState] = React.useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            throwOnError: true,
-          },
-        },
-      }),
-  );
+  const [queryClientState] = React.useState(() => new QueryClient({}));
   return (
     <QueryClientProvider client={queryClientState}>
       {children}
