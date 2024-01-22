178e4b7e8895b91eaf1bb60f53d47ffe98afccc1
devgiljong <calf-love@naver.com>
2024-01-23 01:24:08 +0900

    :sparkles: Add

--- clean/src/@packages/log/core/service.ts
+++ clean/src/@packages/log/core/service.ts
@@ -1,3 +1,10 @@
+import {
+  LogEventName,
+  LogEventNameTuple,
+  LogEventPath,
+  LogEventPathTuple,
+} from './type';
+
 export class LoggerService<
   Feature extends string,
   Page extends string,
@@ -15,21 +22,36 @@ export class LoggerService<
     this.glue = config?.defaultOptions?.glue ?? ('_' as Glue);
   }
 
-  convertEventNameTupleToString(
+  nameTupleToString(
     tuple: [Feature, Target, Action] | readonly [Feature, Target, Action],
-  ) {
-    return tuple.join(
-      this.glue,
-    ) as `${Feature}${Glue}${Target}${Glue}${Action}`;
+  ): LogEventName<Feature, Target, Action> {
+    return tuple.join(this.glue) as LogEventName<Feature, Target, Action>;
   }
 
-  convertEventPathTupleToString(tuple: [Feature, Page, At, Target]) {
-    return tuple.join(
-      this.glue,
-    ) as `${Feature}${Glue}${Page}${Glue}${At}${Glue}${Target}`;
+  pathTupleToString(
+    tuple: [Feature, Page, At, Target],
+  ): LogEventPath<Feature, Page, At, Target> {
+    return tuple.join(this.glue) as LogEventPath<Feature, Page, At, Target>;
   }
 
-  convertEventNameStringToTuple() {}
+  nameStringToTuple(
+    eventName: LogEventName<Feature, Target, Action, Glue>,
+  ): LogEventNameTuple<Feature, Target, Action> {
+    return eventName.split(this.glue) as LogEventNameTuple<
+      Feature,
+      Target,
+      Action
+    >;
+  }
+
+  pathStringToTuple(
+    eventPath: LogEventPath<Feature, Page, At, Target, Glue>,
+  ): LogEventPathTuple<Feature, Page, At, Target> {
+    return eventPath.split(this.glue) as LogEventPathTuple<
+      Feature,
+      Page,
+      At,
+      Target
+    >;
+  }
 }
-
-const hi = new LoggerService<'dsa', 'dsa', 'dsa', 'dsa', 'dsa'>();
--- clean/src/@packages/log/core/type.ts
+++ clean/src/@packages/log/core/type.ts
@@ -13,6 +13,19 @@ export type LogEventPath<
   Glue extends string = string,
 > = `${Feature}${Glue}${Page}${Glue}${At}${Glue}${Target}`;
 
+export type LogEventNameTuple<
+  Feature extends string = string,
+  Target extends string = string,
+  Action extends string = string,
+> = [Feature, Target, Action];
+
+export type LogEventPathTuple<
+  Feature extends string = string,
+  Page extends string = string,
+  At extends string = string,
+  Target extends string = string,
+> = [Feature, Page, At, Target];
+
 export type LogEventProperty<EventProp extends Record<string, any> = {}> =
   EventProp;
 
@@ -24,11 +37,17 @@ export type LogEventEnvironment<Environment extends Record<string, any> = {}> =
 
 export type LogEventUser<User extends Record<string, any> = {}> = User;
 
-export type LogEvent = {
-  eventUser: '';
-  eventProperty: {};
-  eventEnvironment: {};
-  eventName: '';
-  eventPath: '';
+export type LogEvent<
+  Name extends LogEventName,
+  Path extends LogEventPath,
+  User extends LogEventUser,
+  Property extends LogEventProperty,
+  Environment extends LogEventEnvironment,
+> = {
+  eventUser: User;
+  eventProperty: Property;
+  eventEnvironment: Environment;
+  eventName: Name;
+  eventPath: Path;
   eventTime: '';
 };
