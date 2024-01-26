8508bb52b69c83bc183c6b874f4684e63b18927b
devgiljong <calf-love@naver.com>
2024-01-26 18:55:10 +0900

    :sparkles: Add

--- ds/app/page.tsx
+++ ds/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className=" py-16 px-12">
-      <button className=" px-10 w-2/5 py-2  bg-positive text-positive-foreground  ">
+      <button className=" px-10 w-2/5 py-2  bg-positive text-sub-title-1 text-positive-foreground  ">
         hello world
         <div className=" text-foreground/20">dsadsa </div>
       </button>
