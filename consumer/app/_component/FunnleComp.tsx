6f4f36ae9a1e93b3268c80bd099ae46cc55f78f7
devgiljong <calf-love@naver.com>
2024-02-04 14:54:25 +0900

    :sparkles: Device vlrtm

--- consumer/app/_component/FunnleComp.tsx
+++ consumer/app/_component/FunnleComp.tsx
@@ -1,9 +1,24 @@
 'use client';
 import React from 'react';
+import { DeviceHelper } from '@xionhub/device';
 interface FunnleCompProps {}
 
 const FunnleComp = ({}: FunnleCompProps) => {
-  return <div></div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log(DeviceHelper.getDevice());
+          console.log(DeviceHelper.isClient());
+          console.log(DeviceHelper.isIos());
+          const ios = /iPhone|IPad|IPod/i;
+          console.log(ios.test(navigator?.userAgent));
+        }}
+      >
+        클릭하면
+      </button>
+    </div>
+  );
 };
 
 export default FunnleComp;
--- consumer/app/funnel.tsx
+++ consumer/app/funnel.tsx
@@ -1,6 +1,23 @@
+import { useRouter } from 'next/navigation';
+
+type FunnelRouter = {
+  back: () => void;
+  push: (url: string) => void;
+  replace: (url: string) => void;
+};
+
 class Funnel {
   private stack: string[] = [];
   private pageCount: number = 0;
-
-  constructor() {}
+  constructor(private router: FunnelRouter) {}
+  async start(url: string) {
+    this.pageCount = 0;
+    
+  }
+  async end() {}
 }
+
+const useF = () => {
+  const router = useRouter();
+  const funnel = new Funnel(router);
+};
